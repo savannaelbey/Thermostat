8811b7e67f62396fc6b4d83e85ce4abe4f471c90
savannaelbey <savanna_e@icloud.com>
2020-12-04 14:00:25 +0000

    added spec test to thermostatSpec - it has a power saving mode that is on by default- and implemented code to pass the test

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -11,6 +11,9 @@ describe('Thermostat', function() {
 
   it('sets a default temp of 20 degrees celcius', function () {
     expect(thermostat.temp).toEqual(20);
+  });
 
+  it('has a power saving mode that is ON by default', function() {
+    expect(thermostat.powerSavingMode).toBe('ON')
   });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -3,6 +3,8 @@
 class Thermostat {
   constructor() {
     var temperature = new Temperature();
+    var powerSaving = new PowerSaving();
     this.temp = temperature.currentTemp;
+    this.powerSavingMode = powerSaving.mode
   }
 }
